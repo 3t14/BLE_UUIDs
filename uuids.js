42cfb624a975d578a62a01ad7fe32578a2d32d18
Michiru Tanaka <michiru@r4hs.com>
2014-09-18 12:25:53 +0900

    Update uuids.js

--- uuids.js
+++ uuids.js
@@ -3,3 +3,8 @@
 * if you include this JavaScript file, you can use the UUID information of Bluetooth Low Energy devices easily.
 *
 */
+
+var UUID_GENERIC_ACCESS_PROFILE = {
+  'name': '<<Generic Access Profile>>',
+  'uuid': '1800'
+};
